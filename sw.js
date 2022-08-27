f892d427b1ba65cd9617d531792e9cfe3118a51d
liuping <liuping@liuping.win>
2022-08-27 11:21:03 +0800

    fix

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-jsproxy_config=x=>{__CONF__=x;importScripts('assets/bundle.c357b25f.js')};importScripts('conf.js')
+jsproxy_config=x=>{__CONF__=x;importScripts(__FILE__=x.assets_cdn+'bundle.c357b25f.js')};importScripts('conf.js')
